64d1326a0905f613d72e7513d033d1822d1ce98a
Gabriel Holodak <gthepiper@gmail.com>
2019-11-30 14:37:38 -0500

    Clean up macros a bit

--- src/croft/class.cr
+++ src/croft/class.cr
@@ -2,7 +2,10 @@ require "../objc"
 
 module Croft
   abstract class Class
-    @@cls : LibObjc::Class? # Must be nilable?
+    # Get around nilability check for class variables
+    NIL_CLASS = Pointer(Void).new(0).as(LibObjc::Class)
+
+    @@cls : LibObjc::Class = NIL_CLASS
     @obj : LibObjc::Instance
 
     def self.register(name : ::String)
@@ -22,28 +25,33 @@ module Croft
       raise NilAssertionError.new if @obj.null?
     end
 
+    private macro return_helper(return_type)
+      {% if return_type.id == "LibObjc::Instance" %}
+        res.as(LibObjc::Instance)
+      {% elsif return_type.id != "Nil" %}
+        {% unless return_type.id == "Croft::String" %}
+          # Handle a nil string as empty string. Otherwise, raise on nil
+          raise NilAssertionError.new if res.null?
+        {%end%}
+
+        {{return_type}}.new(res.as(LibObjc::Instance))
+      {% end %}
+    end
+
     macro class_method(name, arg_types, return_type, crystal_name = nil)
       {% crystal_name = crystal_name || name.id %}
       {% return_type = return_type || Nil %}
       {% arg_types = arg_types || [] of Nil %}
       def self.{{crystal_name.id}}(
-        {% for i in (0...arg_types.size) %}
-          arg{{i}} : {{arg_types[i]}},
-        {% end %}
+        {% for i in (0...arg_types.size) %} arg{{i}} : {{arg_types[i]}}, {% end %}
       ) : {{ return_type }}
         res = LibObjc.objc_msgSend(
-          @@cls,
+          @@cls.as(Void*),
           Croft::Selector["{{name.id}}"],
-        {% for i in (0...arg_types.size) %}
-            arg{{i}},
-        {% end %}
+          {% for i in (0...arg_types.size) %} arg{{i}}, {% end %}
         )
-        {% if return_type.id == "LibObjc::Instance" %}
-          res.as(LibObjc::Instance)
-        {% elsif return_type != Nil %}
-          raise NilAssertionError.new if res.null?
-          {{return_type}}.new(res.as(LibObjc::Instance))
-        {% end %}
+
+        return_helper({{return_type}})
       end
     end
 
@@ -59,10 +67,8 @@ module Croft
           Croft::Selector["{{name.id}}"],
           {% for i in (0...arg_types.size) %} arg{{i}}, {% end %}
         )
-        {% if return_type.id != "Nil" %}
-          # raise NilAssertionError.new if res.null?
-          {{return_type}}.new(res.as(LibObjc::Instance))
-        {% end %}
+
+        return_helper({{return_type}})
       end
     end
   end
